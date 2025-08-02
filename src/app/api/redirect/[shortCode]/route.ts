57c8203501fc42d7438e1a8e8640f8cd2db7f606
Arun-s21 <arunsng18@gmail.com>
2025-08-02 12:19:10 +0530

    Changed context type to any

--- src/app/api/redirect/[shortCode]/route.ts
+++ src/app/api/redirect/[shortCode]/route.ts
@@ -2,11 +2,11 @@ import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from 'next/server'; 
 const prisma = new PrismaClient();
 
-export async function GET(request:NextRequest, context:{params:{shortCode:string}}){
+export async function GET(request:NextRequest, context:any){
 
     try{
 
-        const shortCode = context.params.shortCode;
+        const shortCode = context.params.shortCode;  //we named the folder[shortCode] so nextjs knows anything after /redirect is shortCode and creates a params object which stores this shortCode variable
  // or const shortCode=context.shortCode params is an object which contains shortCode like context:{shortCode:asda123s}
         const link = await prisma.link.findUnique({
             where:{shortCode}
